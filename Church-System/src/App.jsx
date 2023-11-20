95e07e30e5f0500fe7d180f2659c0707a8924528
peter muthwi <peterkilungya1@gmail.com>
2023-11-20 21:17:10 +0300

    route done

--- Church-System/src/App.jsx
+++ Church-System/src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import ClassOne from './assets/ClassOne';
+import ClassTwo from './assets/ClassTwo';
+import ClassThree from './assets/ClassThree';
+import ClassFour from './assets/ClassFour';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/class1">Class 1</Link>
+          </li>
+          <li>
+            <Link to="/class2">Class 2</Link>
+          </li>
+          <li>
+            <Link to="/class3">Class 3</Link>
+          </li>
+          <li>
+            <Link to="/class4">Class 4</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/class1" element={<ClassOne />} />
+        <Route path="/class2" element={<ClassTwo />} />
+        <Route path="/class3" element={<ClassThree />} />
+        <Route path="/class4" element={<ClassFour />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- Church-System/src/main.jsx
+++ Church-System/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
+function Main() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(<Main />, document.getElementById('root'));
